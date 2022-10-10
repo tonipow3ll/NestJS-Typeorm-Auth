805b4e05ac2c9da2d5630ab093531568840458e6
Toni Powell <tonipow3ll@gmail.com>
2022-10-10 15:13:03 -0400

    all downhill from here

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities:[],
+      // update this
+      synchronize: true
+    }),
+    ConfigModule.forRoot()
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

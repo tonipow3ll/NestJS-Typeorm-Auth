83519ac62a374b1e0c806d7f5bbd080abe7f2eb9
Toni Powell <tonipow3ll@gmail.com>
2022-10-19 17:16:46 -0400

    adds values to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+
+require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.HOST,
       port: 3306,
-      username: '',
-      password: '',
-      database: '',
-      entities:[],
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      // database: process.env.DATABASE,
+      entities:[User],
       // update this
       synchronize: true
     }),
-    ConfigModule.forRoot()
+    // ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],

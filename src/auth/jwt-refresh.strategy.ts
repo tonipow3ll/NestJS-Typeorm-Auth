cfb6ef205bc982ad2f557180e74de26617c532f8
Toni Powell <tonipow3ll@gmail.com>
2024-02-24 14:12:02 -0500

    adds refresh strategy

---
+++ src/auth/jwt-refresh.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { jwtConstants } from "./constants";
+import { Request } from 'express';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh'
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh'),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.accessSecret,
+    });
+  }
+  
+  async validate(payload: any) {
+    console.log('validate in refresh')
+    return { id: payload.sub, username: payload.email }
+
+  }
+
+  // validate(req: Request, payload: any) {
+  //   const refreshToken = req.get('Authorization').replace('Bearer', '').trim()
+  //   return { ...payload, refreshToken }
+  // }
+}
